f9b617e6c285c01d4ba96d7bdd6e35acf2afb2eb
猫猫搞咩啊 <i@neko.ink>
2020-02-18 10:10:13 +0800

    Upload chapter 4 code.

---
+++ Chapter 04/4-21.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector<int> veci{ 0,1,2,3,4,5,6,7,8,9 };
+	for (auto& i : veci)
+	{
+		i = i % 2 == 0 ? i : i * 2;
+	}
+	for (auto i : veci)
+		cout << i << ' ';
+	cout << endl;
+	return 0;
+}
